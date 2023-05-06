3ad5d16c6e148ff2a0ab5c5132acf0523e13b2fb
Tibi <tiberiuiurco@gmail.com>
2023-05-06 22:07:58 +0300

    Added rust lsp & split keymaps

--- lua/tsubo/lsp/init.lua
+++ lua/tsubo/lsp/init.lua
@@ -1,145 +1,152 @@
 local neodev = vim.F.npcall(require, "neodev")
 if neodev then
-  neodev.setup {
-    override = function(_, library)
-      library.enabled = true
-      library.plugins = true
-    end,
-  }
+	neodev.setup {
+		override = function(_, library)
+			library.enabled = true
+			library.plugins = true
+		end,
+	}
 end
 
 local lspconfig = vim.F.npcall(require, "lspconfig")
 if not lspconfig then
-  return
+	return
 end
 
 local is_mac = vim.fn.has "macunix" == 1
 
 local custom_init = function(client)
-    client.config.flags = client.config.flags or {}
-    client.config.flags.allow_incremental_sync = true
+	client.config.flags = client.config.flags or {}
+	client.config.flags.allow_incremental_sync = true
 end
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local opts = {noremap = true, silent = true}
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = {noremap = true, silent = true, buffer = bufnr}
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
-                   bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f',
-                   function() vim.lsp.buf.format {async = true} end, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
+		bufopts)
+	vim.keymap.set('n', '<leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<leader>f',
+		function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 require('lspkind').init({
-    -- DEPRECATED (use mode instead): enables text annotations
-    --
-    -- default: true
-    -- with_text = true,
+	-- DEPRECATED (use mode instead): enables text annotations
+	--
+	-- default: true
+	-- with_text = true,
 
-    -- defines how annotations are shown
-    -- default: symbol
-    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-    mode = 'symbol_text',
-    -- default symbol map
-    -- can be either 'default' (requires nerd-fonts font) or
-    -- 'codicons' for codicon preset (requires vscode-codicons font)
-    --
-    -- default: 'default'
-    preset = 'codicons',
-    -- override preset symbols
-    --
-    -- default: {}
-    symbol_map = {
-        Text = "",
-        Method = "",
-        Function = "",
-        Constructor = "",
-        Field = "ﰠ",
-        Variable = "",
-        Class = "ﴯ",
-        Interface = "",
-        Module = "",
-        Property = "ﰠ",
-        Unit = "塞",
-        Value = "",
-        Enum = "",
-        Keyword = "",
-        Snippet = "",
-        Color = "",
-        File = "",
-        Reference = "",
-        Folder = "",
-        EnumMember = "",
-        Constant = "",
-        Struct = "פּ",
-        Event = "",
-        Operator = "",
-        TypeParameter = ""
-    }
+	-- defines how annotations are shown
+	-- default: symbol
+	-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+	mode = 'symbol_text',
+	-- default symbol map
+	-- can be either 'default' (requires nerd-fonts font) or
+	-- 'codicons' for codicon preset (requires vscode-codicons font)
+	--
+	-- default: 'default'
+	preset = 'codicons',
+	-- override preset symbols
+	--
+	-- default: {}
+	symbol_map = {
+		Text = "",
+		Method = "",
+		Function = "",
+		Constructor = "",
+		Field = "ﰠ",
+		Variable = "",
+		Class = "ﴯ",
+		Interface = "",
+		Module = "",
+		Property = "ﰠ",
+		Unit = "塞",
+		Value = "",
+		Enum = "",
+		Keyword = "",
+		Snippet = "",
+		Color = "",
+		File = "",
+		Reference = "",
+		Folder = "",
+		EnumMember = "",
+		Constant = "",
+		Struct = "פּ",
+		Event = "",
+		Operator = "",
+		TypeParameter = ""
+	}
 })
 
 require('lspconfig')['lua_ls'].setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {
-                    -- Vim
-                    'vim', -- Busted
-                    "describe", "it", "before_each", "after_each", "teardown",
-                    "pending", "clear", -- Colorbuddy
-                    "Color", "c", "Group", "g", "s", -- Custom
-                    "RELOAD"
-                }
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true)
-            }
-        }
-    }
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = {
+					-- Vim
+					'vim', -- Busted
+					"describe", "it", "before_each", "after_each", "teardown",
+					"pending", "clear", -- Colorbuddy
+					"Color", "c", "Group", "g", "s", -- Custom
+					"RELOAD"
+				}
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true)
+			}
+		}
+	}
 }
 require('lspconfig')['tsserver'].setup {
-    capabilities = capabilities,
-    on_attach = on_attach
+	capabilities = capabilities,
+	on_attach = on_attach
 }
 require('lspconfig')['clangd'].setup {
-    capabilities = capabilities,
-    on_attach = on_attach
+	capabilities = capabilities,
+	on_attach = on_attach
+}
+require 'lspconfig'.rust_analyzer.setup {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	cmd = {
+		"rustup", "run", "stable", "rust-analyzer",
+	}
 }
 -- Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require'lspconfig'.html.setup {capabilities = capabilities}
+require 'lspconfig'.html.setup { capabilities = capabilities }
 
 -- ANGULARLS
 -- local project_library_path =
@@ -154,8 +161,8 @@ require'lspconfig'.html.setup {capabilities = capabilities}
 --     on_new_config = function(new_config, new_root_dir) new_config.cmd = cmd end
 -- }
 require('lspconfig')['angularls'].setup({
-    capabilities = capabilities,
-    on_attach = on_attach
+	capabilities = capabilities,
+	on_attach = on_attach
 })
 
 -- MASON + Sumneko_lua
--- plugin/keymaps.vim
+++ plugin/keymaps.vim
@@ -1,2 +1,39 @@
 " Execute this file
 nnoremap <leader><leader>x :call tsubo#save_and_exec()<CR>
+"
+" Easier Moving between splits
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+
+" Make windows to be basically the same size
+nnoremap <leader>= <C-w>=
+
+" Sizing window horizontally
+nnoremap <c-,> <C-W><
+nnoremap <c-.> <C-W>>
+nnoremap <A-,> <C-W>5<
+nnoremap <A-.> <C-W>5>
+
+" Sizing window vertically
+" taller
+nnoremap <A-t> <C-W>+
+" shorter
+nnoremap <A-s> <C-W>-
+
+if has('nvim')
+    " Make esc leave terminal mode
+    " tnoremap <leader><Esc> <C-\><C-n>
+    tnoremap <Esc><Esc> <C-\><C-n>
+
+    " Easy moving between the buffers
+    tnoremap <A-h> <C-\><C-n><C-w>h
+    tnoremap <A-j> <C-\><C-n><C-w>j
+    tnoremap <A-k> <C-\><C-n><C-w>k
+    tnoremap <A-l> <C-\><C-n><C-w>l
+
+    " Try and make sure to not mangle space items
+    tnoremap <S-Space> <Space>
+    tnoremap <C-Space> <Space>
+endif
