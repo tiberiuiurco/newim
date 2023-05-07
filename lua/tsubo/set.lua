16de901ef63b16315497477038667541c66925c8
Tibi <tiberiuiurco@gmail.com>
2023-05-07 14:46:29 +0300

    Keybindings reworked

--- lua/tsubo/set.lua
+++ lua/tsubo/set.lua
@@ -11,14 +11,14 @@ vim.opt.shiftwidth = 2
 vim.opt.laststatus = 3
 
 -- Keybindings
-vim.api.nvim_set_keymap("n", "y", '"+y', {noremap = true})
-vim.api.nvim_set_keymap("v", "y", '"+y', {noremap = true})
-vim.api.nvim_set_keymap("n", "p", '"+p', {noremap = true})
-vim.api.nvim_set_keymap("v", "p", '"+p', {noremap = true})
+vim.api.nvim_set_keymap("n", "y", '"+y', { noremap = true })
+vim.api.nvim_set_keymap("v", "y", '"+y', { noremap = true })
+vim.api.nvim_set_keymap("n", "p", '"+p', { noremap = true })
+vim.api.nvim_set_keymap("v", "p", '"+p', { noremap = true })
 
-vim.api.nvim_set_keymap("n", "dd", '"_dd', {noremap = true})
-vim.api.nvim_set_keymap("v", "dd", '"_dd', {noremap = true})
+vim.api.nvim_set_keymap("n", "dd", '"+dd', { noremap = true })
+vim.api.nvim_set_keymap("v", "dd", '"+dd', { noremap = true })
 
 -- Bind change to discard the content
-vim.api.nvim_set_keymap("n", "c", '"_c', {noremap = true})
-vim.api.nvim_set_keymap("v", "c", '"_c', {noremap = true})
+vim.api.nvim_set_keymap("n", "c", '"_c', { noremap = true })
+vim.api.nvim_set_keymap("v", "c", '"_c', { noremap = true })
