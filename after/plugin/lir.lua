42bafbed4b5e4a069379f8018803170867dae3eb
tiberiuiurco <tiberiuiurco@gmail.com>
2023-05-31 21:29:25 +0300

    lir added

--- after/plugin/lir.lua
+++ after/plugin/lir.lua
@@ -1,51 +1,56 @@
 local has_lir, lir = pcall(require, "lir")
-if not has_lir then
-  return
-end
+if not has_lir then return end
+local mark_actions = require 'lir.mark.actions'
+local clipboard_actions = require'lir.clipboard.actions'
 
 local has_devicons, devicons = pcall(require, "nvim-web-devicons")
 if has_devicons then
-  devicons.setup {
-    override = {
-      lir_folder_icon = {
-        icon = "",
-        color = "#7ebae4",
-        name = "LirFolderNode",
-      },
-    },
-  }
+    devicons.setup {
+        override = {
+            lir_folder_icon = {
+                icon = "",
+                color = "#7ebae4",
+                name = "LirFolderNode"
+            }
+        }
+    }
 end
 
 local actions = require "lir.actions"
 local has_mmv, mmv_actions = pcall(require, "lir.mmv.actions")
 
 lir.setup {
-  show_hidden_files = true,
-  devicons = { enable = true },
+    show_hidden_files = true,
+    devicons = {enable = true},
 
-  float = { winblend = 15 },
+    float = {winblend = 15},
 
-  mappings = {
-    ["l"] = actions.edit,
-    ["h"] = actions.up,
+    mappings = {
+        ["l"] = actions.edit,
+        ["h"] = actions.up,
 
-    ["K"] = actions.mkdir,
-    ["N"] = actions.newfile,
-    ["R"] = actions.rename,
-    ["Y"] = actions.yank_path,
-    ["D"] = actions.delete,
-    ["."] = actions.toggle_show_hidden,
+        ["K"] = actions.mkdir,
+        ["N"] = actions.newfile,
+        ["R"] = actions.rename,
+        ["Y"] = actions.yank_path,
+        ["D"] = actions.delete,
+        ["."] = actions.toggle_show_hidden,
 
-    -- mmv
-    ["M"] = (has_mmv and mmv_actions.mmv) or nil,
-  },
+        -- mmv
+        ["M"] = (has_mmv and mmv_actions.mmv) or nil,
+        ['J'] = function()
+            mark_actions.toggle_mark()
+            vim.cmd('normal! j')
+        end,
+        ['C'] = clipboard_actions.copy,
+        ['X'] = clipboard_actions.cut,
+        ['P'] = clipboard_actions.paste
+    }
 }
 
-require("lir.git_status").setup {
-  show_ignored = false,
-}
+require("lir.git_status").setup {show_ignored = false}
 
-vim.api.nvim_set_keymap("n", "-", ":edit %:h<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "-", ":edit %:h<CR>", {noremap = true})
 
 -- Can do this if we want to get particular settings
 -- vim.cmd [[
